29c604ba9d8c4618fad1cba4c5b072f0805eb481
Nestor Skoczylas <nskoczylas@effect.fr>
2025-07-22 13:34:08 +0200

    chore: register Settings screen in App navigator

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import EditorScreen from './src/screens/EditorScreen';
 import ExecutionScreen from './src/screens/ExecutionScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Accueil' }} />
         <Stack.Screen name="Execution" component={ExecutionScreen} options={{ title: 'Exécution' }} />
         <Stack.Screen name="Editor" component={EditorScreen} options={{ title: 'Éditeur' }} />
+        <Stack.Screen name="Settings" component={SettingsScreen} options={{ title: 'Paramètres' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
